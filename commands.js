b0044acd30cde16abc17f709f34d8f0d3fe8d3ec
saàya <57566725+saaya-code@users.noreply.github.com>
2021-02-13 22:26:45 +0100

    Update commands.js

--- commands.js
+++ commands.js
@@ -9,7 +9,6 @@ const roaster = require("./commands/roaster")
 const quote  = require("./commands/quote")
 const help = require("./commands/help")
 const invite = require("./commands/invite")
-const screenshot = require("./commands/screenshot")
 const commands = {
  ping,select,gif,meme,say,joke,avatar,roaster,quote,help,invite
  
