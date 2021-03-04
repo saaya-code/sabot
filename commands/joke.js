db2986111237464eb00701d9c2409122a111a342
saàya <57566725+saaya-code@users.noreply.github.com>
2021-03-04 17:05:23 +0100

    update

--- commands/joke.js
+++ commands/joke.js
@@ -9,8 +9,6 @@ json = await response.json()
 msg.reply(json.setup)
 setTimeout(() => {
 msg.channel.send(json.punchline)
-console.log('!joke in '+msg.channel.name)
-console.log(json.setup+" "+json.punchline)
 }, 5000);
 }catch(err){console.error(err)
     msg.react("❌")
--- commands/meme.js
+++ commands/meme.js
@@ -6,15 +6,12 @@ word = args.join(" ")
 const url = `https://meme-api.herokuapp.com/gimme/${word}`
 response = await fetch(url);
 json = await response.json();
-console.log(word)
 if (json.code == '404'){
     msg.channel.send(json.message)
 }
 else{
 msg.reply('Here\'s a meme from /r/'+json.subreddit)
-msg.channel.send(json.url)
-console.log('!meme in '+msg.channel.name)
-} 
+msg.channel.send(json.url)} 
 }catch(err){
 console.error(err)
 msg.react("❌")
