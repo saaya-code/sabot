358e0ca70b821758ae4da710ceeab6982ea77ecb
saàya <57566725+saaya-code@users.noreply.github.com>
2021-03-18 14:27:14 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const mongoose = require("mongoose");
+const Topgg = require('@top-gg/sdk')
 require('dotenv').config();
+const api = new Topgg.Api(process.env.TOPGG)
+
 client.login(process.env.TOKEN)
 const commandHandler = require("./commands");
 client.on("message",commandHandler)  
@@ -16,11 +18,16 @@ client.on('ready',()=>{
         const Guilds = client.guilds.cache.map(guild => guild.id) 
         console.log(`Joined new guild: ${g.name}`);
         client.user.setActivity(`Running on ${Guilds.length} servers.| Use "*help","*invite" and "*about".`);
+        api.postStats({
+            serverCount: client.guilds.cache.size
+          })
     });
     client.on("guildDelete",(g) => { 
         const Guilds = client.guilds.cache.map(guild => guild.id) 
         console.log(`left guild: ${g.name}`);
         client.user.setActivity(`Running on ${Guilds.length} servers.| Use "*help","*invite" and "*about".`);
-
+        api.postStats({
+            serverCount: client.guilds.cache.size
+          })
     });
 
