0c33d7dfc02c3f167023db5aaf6a95ed02b61fd9
saàya <57566725+saaya-code@users.noreply.github.com>
2021-03-24 11:13:52 +0100

    updating,about

--- commands/info/about.js
+++ commands/info/about.js
@@ -4,11 +4,12 @@ module.exports = async function(msg){
     var color = msg.member.displayHexColor;
     const embed = new Discord.MessageEmbed()
     .setColor(color)
-    .setImage("https://cdn.discordapp.com/avatars/759795320254496768/61188b7f3cc87950a7b48e2aac0c185e.png")
+    .setThumbnail("https://cdn.discordapp.com/avatars/759795320254496768/61188b7f3cc87950a7b48e2aac0c185e.png")
     .setFooter('Made by saàya#1206')
     .addField('This bot is made for educational purposes any feedback is appreciated.',"\u200B")
     .setTitle("Join our support server !")
     .setURL('https://discord.gg/92nbqStPXm')
+    .addField("For more info visit :","[https://top.gg/bot/759795320254496768](https://top.gg/bot/759795320254496768)")
     msg.channel.send(embed)
 }
 catch(err){
